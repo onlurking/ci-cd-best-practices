e5e4c14ccaa13f0cac2809cc66faceb042c9f552
Diogo Felix <diogo2fex@gmail.com>
2019-11-25 18:09:31 -0300

    feat: add eslint

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
+const express = require('express');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({'status': 'ok'})
-})
+  res.json({ status: 'ok' });
+});
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000')
-})
+  console.log('Listening on port 3000');
+});
